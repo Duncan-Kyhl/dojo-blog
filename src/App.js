ee29c1cc2f02e7fe055944aa9a80e79e0fa6b983
Duncan-Kyhl <Duncan-Kyhl@users.noreply.github.com>
2021-05-09 11:42:06 -0500

    update'

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
 import './App.css';
 
 function App() {
-  const title = 'Welcome to the new blog';
-  const likes = 50;
-  const link = "http://www.google.com";
+
 
   return (
     <div className="App">
       <header className="App-header">
-        <h1>{ title }</h1>
-        <p>Liked { likes } times</p>
-        <a href={ link }>Google Site</a>
+  
       </header>
     </div>
   );
